3a90071bbcf69ebe7df646a87bbf6e244ac25528
Taylor Bird <nsubordin81@gmail.com>
2024-02-19 23:27:39 -0500

    a bunch of metals dot files that I don't know if I should be commiting and the one comment I added

--- src/main/scala/com/example/StupidFizzBuzz.scala
+++ src/main/scala/com/example/StupidFizzBuzz.scala
@@ -22,10 +22,18 @@ import scala.concurrent.duration._
 
 object StupidFizzBuzz extends IOApp.Simple:
   val run =
+    // I understand this, it is a for-yield
+    // one way of looking at this is syntactic sugar over a flatmap??
+    // I forget how 
     for
+      // no idea what IO.ref(0) returns, but looks like a counter
       ctr <- IO.ref(0)
 
+      // the IO sleep looks straightforward
       wait = IO.sleep(1.second)
+      // don't know the *> thing. but intuitively look slike 
+      // "do wait and then get the next  value for assignment and continue"
+      // kind of like forcing streaming to look imperative?
       poll = wait *> ctr.get
 
       _ <- poll.flatMap(IO.println(_)).foreverM.start
